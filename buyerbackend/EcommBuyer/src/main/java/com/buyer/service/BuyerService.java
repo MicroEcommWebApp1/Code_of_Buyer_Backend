ad01d7ffbb634a227e258620a85288f590644719
SHELBY_E_COMM_WEB_APPLICATION <162012715+MicroEcommWebApp1@users.noreply.github.com>
2024-03-12 18:38:56 +0530

    Add files via upload

---
+++ buyerbackend/EcommBuyer/src/main/java/com/buyer/service/BuyerService.java
@@ -0,0 +1,18 @@
+package com.buyer.service;
+
+import org.springframework.http.ResponseEntity;
+
+import com.buyer.dto.BuyerDto;
+import com.buyer.dto.LoginDto;
+
+public interface BuyerService {
+
+	public ResponseEntity<?> buyerRegistration(BuyerDto buyerDto);
+
+	public ResponseEntity<?> buyerLogin(LoginDto login);
+
+	public ResponseEntity<?> forgotPassword(String email,String password);
+
+	public ResponseEntity<?> getBuyerDetails(String email);
+
+}
